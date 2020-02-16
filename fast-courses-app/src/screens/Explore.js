1a7c84d26721b4919f674b09557e3391ad30be08
Rayan <rayankrishnan@live.com>
2020-02-15 23:02:25 -0800

    full screen explore

--- fast-courses-app/src/screens/Explore.js
+++ fast-courses-app/src/screens/Explore.js
@@ -1,6 +1,10 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, Button } from 'react';
 import Modal from 'react-modal';
 import Iframe from 'react-iframe';
+
+function full() {
+  document.getElementById('map_frame').requestFullscreen();
+}
 
 export default ({ history, store }) => {
   return (
@@ -13,16 +17,23 @@ export default ({ history, store }) => {
       <h1>
         Explore <em>alpha</em>
       </h1>
-      <Iframe
-        url="https://pranavvaid.github.io/cardinalgraph/"
-        width="1200px"
-        height="550px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        allowFullScreen
-      />
+      <div>
+        <Iframe
+          url="https://pranavvaid.github.io/cardinalgraph/"
+          width="100%"
+          height="650"
+          id="map_frame"
+          className="mapFrame"
+          display="initial"
+          position="relative"
+          allowFullScreen
+          scrolling="yes"
+        />
+      </div>
+
+      <button onClick={full} class="ais-ClearRefinements-button">
+        Full Screen!
+      </button>
     </Modal>
   );
 };
